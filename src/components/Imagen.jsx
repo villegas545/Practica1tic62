2c7cd19ea43a5ff23564d91f39dd067bac3e4abf
villegas545 <villegas_545@hotmail.com>
2021-05-22 16:46:35 -0600

    Rama master

---
+++ src/components/Imagen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Imagen() {
+    return (
+        <div>
+            <img src="https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen-800x419.jpg" alt="" />   
+        </div>
+    )
+}
+
+export default Imagen
+    
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Parrafo() {
+    return (
+        <div>
+            <h5>Hola villegas</h5>
+            <p>Esto es un parrafo</p>
+            <ul>
+                <li>
+                    Lista1
+                </li>
+                <li>
+                    Lista2
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Parrafo
